e13deded743252d015db6d3aaead7b78734d934e
MarcoMeyer1 <marcomeyerbusiness@gmail.com>
2024-11-08 00:35:56 +0200

    fixed routes

--- employee/src/App.js
+++ employee/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Login from './Pages/Login';
+import EmployeeDashboard from './Pages/EmployeeDashboard';
+import VerifyTransactions from './Pages/VerifyTransactions';
 
 function App() {
   return (
    <Router>
     <Routes>
     <Route path="/" element={<Login />} />
+    <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+    <Route path="/verify-transactions" element={<VerifyTransactions />} />
     </Routes>
    </Router>
   );
