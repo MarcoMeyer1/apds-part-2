71dfb92fa2a20b1e4dad3cccc418d3c8d722ae41
MarcoMeyer1 <marcomeyerbusiness@gmail.com>
2024-11-11 14:01:46 +0200

    fixed imports for tests

--- employee/src/App.test.js
+++ employee/src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import Login from './Login';
+import Login from './Pages/Login';
 
 test('renders login page', () => {
   render(
